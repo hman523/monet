ba6ab86e6f1cc7415d87fa680d6e1d79c6f38729
hman523 <shbarbella@gmail.com>
2019-06-30 15:44:07 -0400

    Renamed the project to Monet

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BIPL
+# Monet
 A Basic Inspired Programming Language
 
 This is my attempt at writing a language. While it will be a simple one, I am just doing this to learn and maybe make a useful scripting tool. Ultimately, I have no plans for this except learning from it.
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "Interpreter.h"
 #include <iostream>
 
-// Welcome to BIPL (Basic Inspired Programming Language)
+// Welcome to Monet (A Basic Inspired Programming Language)
 
 int main(int argc, char *argv[]) {
-  std::cout << "Welcome to the BIPL Interpreter" << std::endl;
+  std::cout << "Welcome to the Monet Interpreter" << std::endl;
   if (argc != 2) {
     std::cerr << "Need a file to interpret" << std::endl;
     exit(1);
