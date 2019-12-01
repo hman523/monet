5276aa963e41ac424fe973a2c74e6a9c2964f775
Baibhav Vatsa <baibhavvatsa@gmail.com>
2019-12-01 16:17:46 -0600

    fixed bugs with eval and removed type checking for port number

--- lib/UDP/UDP.cpp
+++ lib/UDP/UDP.cpp
@@ -25,21 +25,23 @@ std::string UDP::eval(const std::string &expression) {
   throw Exception("Fatal implementation error in UDP library in UDP.cpp");
 }
 
-int UDP::evalToInt(const std::string &val) {
+int UDP::evalint(const std::string &val) {
   return Interpreter::Instance()->strToInt(val);
 }
 
-bool UDP::isNum(const std::string &val) {
-  return Interpreter::Instance()->isNumber(val);
+std::string UDP::evalstring(const std::string &val) {
+  if (Interpreter::Instance()->isParens(val)) {
+    return Interpreter::Instance()->eval(val);
+  } else if (Interpreter::Instance()->isString(val)) {
+    return Interpreter::Instance()->removequotes(val);
+  } else {
+    return Interpreter::Instance()->strtostr(val);
+  }
 }
 
 std::string UDP::send(const std::vector<std::string> &expression) {
   if (expression.size() < 4) {
     throw Exception("Wrong number of parameters for udp.send");
-  }
-
-  if (!isNum(expression[2])) {
-    throw Exception("Wrong type in second parameter of udp.send");
   }
 
   boost::asio::io_context io_service;
@@ -54,24 +56,31 @@ std::string UDP::send(const std::vector<std::string> &expression) {
         "Invalid IP address received in first parameter of udp.send");
   }
 
-  int port = evalToInt(expression[2]);
+  int port = evalint(expression[2]);
   if (port < 0) {
     throw Exception(
-        "Invalid port number received in second parameter of udp.send");
+        "Invalid port number received in second papression[1rameter of udp.send");
   }
+
   boost::asio::ip::udp::endpoint ep(ip_address, port);
+  
   try {
     socket.connect(ep);
   } catch (boost::system::system_error &e) {
     throw Exception("udp.send failed to connect socket to endpoint " +
                     expression[1] + ":" + std::to_string(port));
   }
+  
   std::string message = expression[3];
+  if (expression.size() == 4) {
+    message = evalstring(message);
+  } else {
   message = std::accumulate(expression.begin() + 4, expression.end(), message,
                             [](const std::string &m1, const std::string &m2) {
                               return m1 + " " + m2;
                             }) +
             "\n";
+  }
   boost::system::error_code error;
 
   socket.send(boost::asio::buffer(message), 0, error);
@@ -88,12 +97,8 @@ std::string UDP::rec(const std::vector<std::string> &expression) {
     throw Exception("Wrong number of parameters for udp.receive");
   }
 
-  if (!isNum(expression[1])) {
-    throw Exception("Wrong type in first parameter of udp.receive");
-  }
-
   boost::asio::io_context io_service;
-  int port = evalToInt(expression[1]);
+  int port = evalint(expression[1]);
   if (port < 0) {
     throw Exception(
         "Invalid port number received in first parameter of udp.send");
